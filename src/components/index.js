034179e2223615e70f8d637cf9b2e7e31f3644e9
Suthar Darshan <suthardarshan2782@gmail.com>
2024-06-06 18:53:39 +0530

    Update Components Index file

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container.jsx";
-import LogoutBtn from "./Header/LogoutBtn.jsx";
-import Logo from "./Logo.jsx";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Button from './Button'
+import Select from './Select'
+import Input from './Input'
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, LogoutBtn, Logo }
+export { Header, Footer, Container, LogoutBtn, Logo, Button, Select, Input, PostCard }
